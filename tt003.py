0b18708d48bb77d70cc7fd14814373c319405b92
jason.jafari.m@gmail.com <jason.jafari.m@gmail.com>
2020-05-03 19:48:40 -0400

    ggg

--- tt003.py
+++ tt003.py
@@ -1,12 +1,12 @@
 from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
- 
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html" ,content="testing")
- 
 
-if __name__== "__main__":
-    app.run()
+@app.route("/")
+def home():
+    return render_template("index.html", content="testing")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
